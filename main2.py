78561b5fb3c83de3a39581358edb9d3070ee4620
aagr29 <63660748+aagr29@users.noreply.github.com>
2021-07-16 11:34:09 +0930

    clean up

--- main2.py
+++ main2.py
@@ -8,8 +8,12 @@ def shortestpath(tx, node1, node2):
         result = tx.run("MATCH p=shortestpath((start:Nodeid{id:$node1})-[:edgeId*]- (end:Nodeid{id:$node2}))UNWIND relationships(p) AS  e return start.id,end.id, sum(e.Length) as summation", node1=node1, node2=node2)
         print(result.single())
         
+print("shortest path finder between two nodes in the network")
 
+
+print('Input first node:'),
 a=int(input())
+print('Input second node:'),
 b=int(input())
 with driver.session() as session:
     session.write_transaction(shortestpath,a,b)
